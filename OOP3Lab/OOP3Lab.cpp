d2b39db26b320fd66f3acfe34300b8d75d22bdeb
javantlang <javantlang@gmail.com>
2020-10-25 21:32:11 +0500

    Добавил класс Node и класс List

--- OOP3Lab/OOP3Lab.cpp
+++ OOP3Lab/OOP3Lab.cpp
@@ -2,6 +2,51 @@
 
 using namespace std;
 
+class Node {
+public:
+    Node* next;
+    Node* prev;
+    Node()
+    {
+        prev = NULL;
+        next = NULL;
+    }
+    friend class List;
+    virtual void getName()
+    {
+        cout << "Node\n";
+    }
+};
+
+class List {
+    Node* root;
+    Node* tail;
+    Node* current;
+    int count = 0;
+public:
+    List() {
+        cout << "constructor List\n";
+        current = NULL;
+        root = NULL;
+        tail = root;
+    }
+    int getCount() { return count; };
+    bool isEmpty() { return root == NULL; };
+    void first() { current = root; }
+    void last() { current = tail; };
+    void next() { current = current->next; }
+    void prev() { current = current->prev; }
+    Node* getObject() { return current; }
+    bool eol() { return current == NULL; }
+    template <class T>
+    //Добавление элемента в начало списка
+    Node* Add(T*);
+    template <class T>
+    //Добавление элемента после передаваемого указателя на элемент
+    Node* Add(T*, Node*);
+    Node* Delete(Node*);
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
